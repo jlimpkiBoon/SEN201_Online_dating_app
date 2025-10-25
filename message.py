0ed3d667889e4b942697e9ecbfa59727a2488cdf
PepperX1 <bawornpat12365@gmail.com>
2025-10-25 12:55:35 +0700

    Add notification integration to message.py
    - Integrated create_notification() into send_message()
    - Ensures each message automatically creates a notification for the receiver
    - Prepares backend for notifications table in database.py

--- message.py
+++ message.py
@@ -1,28 +1,39 @@
 import sqlite3
 from database import get_conn, init_db
 from datetime import datetime
+from notification import create_notification  
 
 def send_message(sender, receiver, content):
     init_db()
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute
-    (
+    
+    # insert the message
+    cur.execute(
         "INSERT INTO messages (username, receiver, content) VALUES (?, ?, ?);",
         (sender, receiver, content)
     )
     conn.commit()
     conn.close()
+    
+    #create notification for receiver
+    create_notification(
+        username=receiver,
+        notif_type="new_message",
+        from_user=sender,
+        content_preview=content[:50]   # only first 50 chars
+    )
+    
     print("Message sent successfully!")
 
 def view_messages(sender, receiver):
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute
-    (
+    
+    cur.execute(
         "SELECT * FROM messages WHERE (username = ? AND receiver = ?) ORDER BY timestamp;",
         (sender, receiver)
     )
     messages = cur.fetchall()
     conn.close()
-    return messages  
+    return messages
